db83c13837b9058d153e09999390624aac70adcd
Unisha Khatiwada <unishakhatiwada1@gmail.com>
2024-09-17 22:05:53 +0545

    make request for update and store

---
+++ app/Http/Requests/DoctorUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DoctorUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /** 
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:doctors,email',
+            'phone' => 'required|string|unique:doctors,phone',
+            'address' => 'required|string',
+            'status' => 'required|in:active,inactive',
+        ];
+    }
+}
