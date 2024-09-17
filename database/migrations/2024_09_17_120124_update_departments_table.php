152a315e6fdb6a6f6386957accf0fb9f6ca6ae02
Unisha Khatiwada <unishakhatiwada1@gmail.com>
2024-09-17 21:51:05 +0545

    create migration for department

--- database/migrations/2024_09_17_120124_update_departments_table.php
+++ database/migrations/2024_09_17_120124_update_departments_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('departments', function (Blueprint $table) {
-            $table->id();
-            $table->string('code')->unique();
-            $table->string('name');
-            $table->string('description')->nullable();
-            $table->timestamps();
+        Schema::table('departments', function (Blueprint $table) {
+            $table->foreignId('doctor_id')
+                ->after('id')
+                ->constrained(table: 'doctors')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
         });
     }
 
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('departments');
+        //
     }
 };
