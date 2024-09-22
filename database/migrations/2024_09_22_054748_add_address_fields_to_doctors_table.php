004a15b1cdf58c6f53c846cde5afe19b790ca89f
Unisha Khatiwada <unishakhatiwada1@gmail.com>
2024-09-22 11:35:34 +0545

    add foreign key for address in the doctor schema

---
+++ database/migrations/2024_09_22_054748_add_address_fields_to_doctors_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+     public function up(): void
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->foreignId('province_id')
+                ->nullable()
+                ->constrained('provinces')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('district_id')
+                ->nullable()
+                ->constrained('districts')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('municipality_id')
+                ->nullable()
+                ->constrained('municipalities')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::table('doctors', function (Blueprint $table) {
+            $table->dropForeign(['province_id']);
+            $table->dropColumn('province_id');
+
+            $table->dropForeign(['district_id']);
+            $table->dropColumn('district_id');
+
+            $table->dropForeign(['municipality_id']);
+            $table->dropColumn('municipality_id');
+        });
+    }
+};
