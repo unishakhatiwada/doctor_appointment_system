6c080efde47276593654078a0b2010f5b3c6c92e
Unisha Khatiwada <unishakhatiwada1@gmail.com>
2024-09-23 15:50:43 +0545

    fix nav top

--- resources/views/admin/layouts/nav-top.blade.php
+++ resources/views/admin/layouts/nav-top.blade.php
@@ -4,10 +4,12 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{route('admin.dashboard')}}" class="nav-link">Home</a>
-        <li class="nav-item btn-info d-none d-sm-inline-block">
-            <a href="{{route('admin.logout')}}" class="nav-link">Logout</a>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item btn btn-info d-none d-sm-inline-block">
+            <a href="{{ route('admin.logout') }}" class="nav-link text-white">Logout</a>
         </li>
     </ul>
 </nav>
